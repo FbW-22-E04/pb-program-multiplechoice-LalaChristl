5cebf8ecbab8416f9d5263110b1f694eedea83a4
LalaChristl <lesleyannchristl@gmail.com>
2022-07-12 13:56:24 +0200

    added JS

--- solution.js
+++ solution.js
@@ -1,1 +1,78 @@
 // write your code here
+const color = "yellow";
+
+switch (color) {
+  case "red":
+    console.log("Q1: Red's a great color on you.");
+    break;
+  case "blue":
+    console.log("Q1: Blue? Great choice.");
+    break;
+  case "green":
+    console.log("Q1: Ok. Green it is.");
+    break;
+  case "yellow":
+    console.log("Q1: Not what I would have picked, but yellow is fine.");
+    break;
+  default:
+    console.log("Q1: The aim of the game is to pick a color.");
+}
+
+console.log("------------------------------------");
+
+const score = 95;
+
+switch (true) {
+  case score >= 90:
+    console.log("Q2: A - Well done!");
+    break;
+  case score >= 80 && score < 90:
+    console.log("Q2: B - Good on you!");
+    break;
+  case score >= 70 && score < 80:
+    console.log("Q2: C - Good effort");
+    break;
+  case score >= 60 && score < 70:
+    console.log("Q2: D - Try harder next time...");
+    break;
+  default:
+    console.log("Q2: Yikes...");
+    break;
+}
+
+console.log("------------------------------------");
+
+const fruit = "strawberry";
+
+switch (fruit) {
+  case "apple":
+    console.log("Q3: An apple a day keeps the doctor away.");
+    break;
+  case "banana":
+    console.log("Q3: Bananas are full of potassium.");
+    break;
+  case "orange":
+    console.log("Q3: Yep, oranges. Great choice.");
+    break;
+  case "strawberry":
+    console.log("Q3: Mmmm...nothing better than strawberries.");
+    break;
+  default:
+    console.log("Q3: PICK A FRUIT");
+}
+
+console.log("------------------------------------");
+const percentageComplete = 99;
+
+if (percentageComplete < 30) {
+  console.log("Q4: Still a long way to go");
+} else if (percentageComplete >= 30 && percentageComplete <= 50) {
+  console.log("Q4: Slowly getting there");
+} else if (percentageComplete >= 51 && percentageComplete <= 80) {
+  console.log("Q4: You can do it!");
+} else if (percentageComplete >= 81 && percentageComplete <= 99) {
+  console.log("Q4: This is the last push!");
+} else {
+  percentageComplete == 100;
+  console.log("Q4: You're there. Well done!");
+}
